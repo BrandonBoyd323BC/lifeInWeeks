d1b628defd65c86c156429462d445b08229e5df6
BrandonBoyd323BC <49135796+BrandonBoyd323BC@users.noreply.github.com>
2021-01-27 20:29:12 -0800

    add files to make the calculator work

---
+++ js/calculate.js
@@ -0,0 +1,9 @@
+const myFunction = () => {
+    let age = document.getElementById("age").value
+    let output = document.getElementById("output")
+    let daysOutput = (90 - age) * 365
+    let weeksOutput = (90 - age) * 52
+    let monthsOutput = (90 - age) * 12
+    output.innerHTML = `You have ${daysOutput} days, ${weeksOutput} weeks, and ${monthsOutput} months left.`
+    
+}
